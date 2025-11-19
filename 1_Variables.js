dff440f908d3aab989ed7fb73d129ef8d5565254
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-11-19 20:57:53 +0530

    Variables types

---
+++ 1_Variables.js
@@ -0,0 +1,20 @@
+
+var age;
+age = 23;
+age = 32;
+var age = 99;   // accept dupalication
+console.log(age);
+
+let old;
+old= 75;
+old = 23;
+// let old = 89;   // cannot redeclare block-scoped variable.
+console.log(old);
+
+
+// const young;   // declaration must be initialize at that time
+const young = 90;
+// young = 32;    cannot re-assigned
+const young = 100; // cannot redeclare block-scoped variable
+console.log(young);
+
