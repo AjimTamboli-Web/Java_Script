b4c128b97f6c49014f1ed7eb46c69f675fad7df1
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-11-20 05:39:27 +0530

    with Details

--- 2_Operators.js
+++ 2_Operators.js
@@ -1,4 +1,8 @@
-// 1. Arithmetic Operators
+// Operators in JavaScript -> which is works on operands to produce a result.Do some operations on variables and values.
+
+
+// 1. Arithmetic Operators  -> +, -, *, /, %, **, ++, --
+
 let a = 10;
 let b = 3;
 
@@ -7,13 +11,27 @@ console.log("Subtraction:", a - b);
 console.log("Multiplication:", a * b);
 console.log("Division:", a / b);
 console.log("Modulus:", a % b);
+
+// Exponential opertator which is used to raise the base to the exponent power.
 console.log("Exponent:", a ** b);
 
+// Increment and Decrement Operators which is used to increase or decrease the value by 1.
 let z = 5;
 console.log("Increment:", ++z);
 console.log("Decrement:", --z);
+// Using post-increment and post-decrement, the current value is returned first, then the increment or decrement operation is performed.
+let s = 5;
+console.log("Post-Increment:", s++);
+console.log("Value after Post-Increment:", s);
+console.log("Post-Decrement:", s--);
+console.log("Value after Post-Decrement:", s);
 
-// 2. Assignment Operators
+// It follows the Bodmas Rule -> Brackets, Orders (i.e. powers and square roots, etc.), Division and Multiplication, Addition and Subtraction.
+let result = (a + b) * (a - b) / b;
+console.log("Result following Bodmas Rule:", result);
+
+
+// 2. Assignment Operators -> =, +=, -=, *=, /=, %= also called as shorthand operators
 
 let num = 10;
 
@@ -25,7 +43,7 @@ num %= 4;   // num = 2
 
 console.log("Final value:", num);
 
-// 3. Comparison Operators
+// 3. Comparison Operators -> ==, ===, !=, !==, >, <, >=, <= ouptputs boolean values (true or false)
 
 let x = 10;
 let y = "10";
@@ -39,7 +57,7 @@ console.log("a < 5:", x < 5);
 console.log("a >= 10:", x >= 10);
 console.log("a <= 10:", x <= 10);
 
-// 4. Logical Operators
+// 4. Logical Operators -> &&, ||, !
 
 let p = true;
 let q = false;
@@ -48,7 +66,11 @@ console.log("AND:", p && q);
 console.log("OR:", p || q);
 console.log("NOT:", !p);
 
-// 5. Ternary Operator
+// 5. Ternary Operator -> condition ? expr1 : expr2
+
+let age1 = 20;
+let canVote = (age1 >= 18) ? "Yes, can vote" : "No, cannot vote";
+console.log("Can vote:", canVote);
 
 let m = 5;   // 0101
 let n = 3;   // 0011
@@ -60,7 +82,7 @@ console.log("Bitwise NOT:", ~m);
 console.log("Left Shift:", m << 1);
 console.log("Right Shift:", m >> 1);
 
-// 6. Typeof Operator
+// 6. Typeof Operator -> to know the data type of a variable or value
 
 let name = "Azim";
 let num2 = 100;
